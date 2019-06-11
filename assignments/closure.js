74b06980ecdce4277495d901437d036b623c65aa
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-06-11 19:11:44 -0400

    Javascript II so far...

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const me = "Jerry";
+
+function callJerry () {
+  return me;
+}
+
+console.log(callJerry());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
